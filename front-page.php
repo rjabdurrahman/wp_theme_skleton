cd9465e88cc4dd90b644bbe6e6933608205a33a3
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-07-08 13:21:27 +0600

    Added: New template files

--- front-page.php
+++ front-page.php
@@ -1,33 +1,61 @@
 <?php get_header();?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown link
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-        </ul>
+<div class="my-container mont-font">
+        <div class="middle-container d-flex justify-content-center">
+            <div class="container sec-container sc-first">
+                <div class="heading ex-font">Enjoy Tech. Enjoy Investing.</div>
+                <p>0 Commissions and no deposit minimums.<br>
+                    Everyone gets smart tools for smart investing.
+                </p>
+                <button type="button" class="btn bg-mypri">Get Started</button>
+            </div>
+            <div class="container sec-container sc-second">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Username or Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+        </div>
+        <div class="bottom-wave"></div>
     </div>
-</nav>
+    <div class="part-2 d-flex justify-content-center">
+        <div>
+            <iframe src="//fast.wistia.net/embed/iframe/avk9twrrbn" allowtransparency="true" frameborder="0"
+                scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen
+                webkitallowfullscreen oallowfullscreen msallowfullscreen width="700" height="400"></iframe>
+            <script src="//fast.wistia.net/assets/external/E-v1.js" async></script>
+        </div>
+        <div style="width: 650px; margin: auto 120px;">
+            <h1 class="ex-font">Build Your Career, A Complete Guideline</h1>
+            <p>
+                We offer a lot of courses and modules for real development. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Eveniet magnam est minus nemo maxime odit amet obcaecati ex magni modi.
+            </p>
+        </div>
+    </div>
+    <div class="part-3 d-flex justify-content-around ex-font">
+        <div>
+            <img src="<?php echo get_template_directory_uri() . 'img/icon-1.png'; ?>" alt="">
+            <p>Get Started and Be Connected</p>
+        </div>
+        <div>
+            <img src="<?php echo get_template_directory_uri() . 'img/icon-2.svg'; ?>" alt="">
+            <p>Join with Community</p>
+        </div>
+        <div>
+            <img src="<?php echo get_template_directory_uri() . 'img/icon-3.svg'; ?>" alt="">
+            <p>Be Certified and Contribute</p>
+        </div>
+    </div>
 <?php get_footer();?>
--- functions.php
+++ functions.php
@@ -3,6 +3,14 @@ function load_css()
 {
     wp_register_style('bootstrap', get_template_directory_uri() . '/vendors/bootstrap/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
+    wp_register_style('style', get_template_directory_uri() . '/css/style.css', array(), false, 'all');
+    wp_enqueue_style('style');
+    wp_register_style('responsive', get_template_directory_uri() . '/css/responsive.css', array(), false, 'all');
+    wp_enqueue_style('responsive');
+    wp_register_style('fa', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css', array(), false, 'all');
+    wp_enqueue_style('fa');
+    wp_register_style('googlefonts', 'https://fonts.googleapis.com/css?family=Exo+2:500,700|Montserrat:300|Roboto|Lato:400,700|Pacifico|Barlow+Condensed:400,500,700&display=swap', array(), false, 'all');
+    wp_enqueue_style('googlefonts');
 }
 
 add_action('wp_enqueue_scripts', 'load_css');
