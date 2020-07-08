75a3044047e035aed26ec46683802d67d7de4d76
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-07-08 13:42:58 +0600

    Arragended: Code for header footer and font-page'

--- footer.php
+++ footer.php
@@ -1,76 +1,78 @@
 <footer>
-        <div class="d-flex flex-row">
-            <div style="margin-right: 60px;">
-                <img src="<?php echo get_template_directory_uri() . '/img/brand.png'; ?>" alt="Brand" height="50" width="180" srcset="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis libero rem repellat odio incidunt
-                    rerum iste distinctio, aperiam ea iure! <br>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam provident quaerat ipsam
-                    laudantium itaque pariatur atque reiciendis tenetur rem ullam.<br>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores rem magnam repellendus.
-                </p>
-            </div>
+    <div class="d-flex flex-row">
+        <div style="margin-right: 60px;">
+            <img src="<?php echo get_template_directory_uri() . '/img/brand.png'; ?>" alt="Brand" height="50"
+                width="180" srcset="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis libero rem repellat odio incidunt
+                rerum iste distinctio, aperiam ea iure! <br>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam provident quaerat ipsam
+                laudantium itaque pariatur atque reiciendis tenetur rem ullam.<br>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores rem magnam repellendus.
+            </p>
+        </div>
+        <div>
+            <h4 style="padding-left: 1rem;">Company Links</h4>
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Partners</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Terms of Use</a>
+                </li>
+            </ul>
+        </div>
+        <div>
             <div>
-                <h4 style="padding-left: 1rem;">Company Links</h4>
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">News</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Partners</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">FAQ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Terms of Use</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <div>
-                    <h4>Newsletter</h4>
-                    <div class="input-group mb-3 newsletter">
-                        <input type="text" class="form-control" placeholder="Enter your email address"
-                            aria-label="Enter your email address" aria-describedby="button-addon2">
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="button" id="button-addon2">Subscribe</button>
-                        </div>
+                <h4>Newsletter</h4>
+                <div class="input-group mb-3 newsletter">
+                    <input type="text" class="form-control" placeholder="Enter your email address"
+                        aria-label="Enter your email address" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button" id="button-addon2">Subscribe</button>
                     </div>
                 </div>
-                <div>
-                    <h4>Follow Us</h4>
-                    <nav class="nav follow">
-                        <a class="nav-link" href="#"><i class="fa fa-facebook-square"></i></a>
-                        <a class="nav-link" href="#"><i class="fa fa-twitter-square"></i></a>
-                        <a class="nav-link" href="#"><i class="fa fa-linkedin"></i></a>
-                        <a class="nav-link" href="#"><i class="fa fa-github-square"></i></a>
-                        <a class="nav-link" href="#"><i class="fa fa-google-plus-square"></i></a>
-                    </nav>
-                </div>
+            </div>
+            <div>
+                <h4>Follow Us</h4>
+                <nav class="nav follow">
+                    <a class="nav-link" href="#"><i class="fa fa-facebook-square"></i></a>
+                    <a class="nav-link" href="#"><i class="fa fa-twitter-square"></i></a>
+                    <a class="nav-link" href="#"><i class="fa fa-linkedin"></i></a>
+                    <a class="nav-link" href="#"><i class="fa fa-github-square"></i></a>
+                    <a class="nav-link" href="#"><i class="fa fa-google-plus-square"></i></a>
+                </nav>
             </div>
         </div>
-        <div class="last">Copyright&copy; 2020 Company. All rights reserved.</div>
-    </footer>
-    <?php wp_footer();?>
-    <script>
-        var navBar = document.querySelector('.navbar');
-        function scrollManage() {
-            if (window.pageYOffset > 240) {
-                navBar.classList.remove("navbar-gen")
-                navBar.classList.add("navbar-st")
-            }
-            else {
-                navBar.classList.remove("navbar-st")
-                navBar.classList.add("navbar-gen")
-            }
-        }
-        window.addEventListener('scroll', scrollManage, true)
-    </script>
+    </div>
+    <div class="last">Copyright&copy; 2020 Company. All rights reserved.</div>
+</footer>
+<?php wp_footer();?>
+<script>
+var navBar = document.querySelector('.navbar');
+
+function scrollManage() {
+    if (window.pageYOffset > 240) {
+        navBar.classList.remove("navbar-gen")
+        navBar.classList.add("navbar-st")
+    } else {
+        navBar.classList.remove("navbar-st")
+        navBar.classList.add("navbar-gen")
+    }
+}
+window.addEventListener('scroll', scrollManage, true)
+</script>
 </body>
+
 </html>
--- front-page.php
+++ front-page.php
@@ -1,61 +1,61 @@
 <?php get_header();?>
 <div class="my-container mont-font">
-        <div class="middle-container d-flex justify-content-center">
-            <div class="container sec-container sc-first">
-                <div class="heading ex-font">Enjoy Tech. Enjoy Investing.</div>
-                <p>0 Commissions and no deposit minimums.<br>
-                    Everyone gets smart tools for smart investing.
-                </p>
-                <button type="button" class="btn bg-mypri">Get Started</button>
-            </div>
-            <div class="container sec-container sc-second">
-                <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Username or Email</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Remember me</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
-            </div>
-        </div>
-        <div class="bottom-wave"></div>
-    </div>
-    <div class="part-2 d-flex justify-content-center">
-        <div>
-            <iframe src="//fast.wistia.net/embed/iframe/avk9twrrbn" allowtransparency="true" frameborder="0"
-                scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen
-                webkitallowfullscreen oallowfullscreen msallowfullscreen width="700" height="400"></iframe>
-            <script src="//fast.wistia.net/assets/external/E-v1.js" async></script>
-        </div>
-        <div style="width: 650px; margin: auto 120px;">
-            <h1 class="ex-font">Build Your Career, A Complete Guideline</h1>
-            <p>
-                We offer a lot of courses and modules for real development. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Eveniet magnam est minus nemo maxime odit amet obcaecati ex magni modi.
+    <div class="middle-container d-flex justify-content-center">
+        <div class="container sec-container sc-first">
+            <div class="heading ex-font">Enjoy Tech. Enjoy Investing.</div>
+            <p>0 Commissions and no deposit minimums.<br>
+                Everyone gets smart tools for smart investing.
             </p>
+            <button type="button" class="btn bg-mypri">Get Started</button>
+        </div>
+        <div class="container sec-container sc-second">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Username or Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
         </div>
     </div>
-    <div class="part-3 d-flex justify-content-around ex-font">
-        <div>
-            <img src="<?php echo get_template_directory_uri() . '/img/icon-1.png'; ?>" alt="">
-            <p>Get Started and Be Connected</p>
-        </div>
-        <div>
-            <img src="<?php echo get_template_directory_uri() . '/img/icon-2.svg'; ?>" alt="">
-            <p>Join with Community</p>
-        </div>
-        <div>
-            <img src="<?php echo get_template_directory_uri() . '/img/icon-3.svg'; ?>" alt="">
-            <p>Be Certified and Contribute</p>
-        </div>
+    <div class="bottom-wave"></div>
+</div>
+<div class="part-2 d-flex justify-content-center">
+    <div>
+        <iframe src="//fast.wistia.net/embed/iframe/avk9twrrbn" allowtransparency="true" frameborder="0" scrolling="no"
+            class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen
+            oallowfullscreen msallowfullscreen width="700" height="400"></iframe>
+        <script src="//fast.wistia.net/assets/external/E-v1.js" async></script>
     </div>
+    <div style="width: 650px; margin: auto 120px;">
+        <h1 class="ex-font">Build Your Career, A Complete Guideline</h1>
+        <p>
+            We offer a lot of courses and modules for real development. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eveniet magnam est minus nemo maxime odit amet obcaecati ex magni modi.
+        </p>
+    </div>
+</div>
+<div class="part-3 d-flex justify-content-around ex-font">
+    <div>
+        <img src="<?php echo get_template_directory_uri() . '/img/icon-1.png'; ?>" alt="">
+        <p>Get Started and Be Connected</p>
+    </div>
+    <div>
+        <img src="<?php echo get_template_directory_uri() . '/img/icon-2.svg'; ?>" alt="">
+        <p>Join with Community</p>
+    </div>
+    <div>
+        <img src="<?php echo get_template_directory_uri() . '/img/icon-3.svg'; ?>" alt="">
+        <p>Be Certified and Contribute</p>
+    </div>
+</div>
 <?php get_footer();?>
